719f6322466b0e4c5a37524aeee1a747de265809
unknown <wangjian_1998@outlook.com>
2018-02-16 18:30:38 +0800

    'bank'

---
+++ exercise-8/bank/src/com/company/Bank.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Bank {
+    double account;
+   String accountName;
+    String accountAdress;
+    void setAccount(double a){
+         account = a;
+    }
+    double getAccount(){
+        return account;
+    }
+    void setAccountName(String  b){
+        accountName=b;
+    }
+    String getAccountName(){
+        return accountName;
+    }
+    void setAccountAdress(String c){
+        accountAdress=c;
+    }
+    String getAccountAdress(){
+        return accountAdress;
+    }
+
+
+}
